abbe168aea75989cbf0dbd9028fb1f50002352f1
ffilipovicc98 <ffilipovicc98@gmail.com>
2023-07-22 12:07:12 +0200

    feature: Adds Tooltip and formatting-service for formating prices and addresses

--- apps/mina-vest/src/components/pages/HomePage/HomePage.tsx
+++ apps/mina-vest/src/components/pages/HomePage/HomePage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
+import { Icon, Tooltip } from "@components/atoms";
 import { Menu } from "@components/organisms/Menu";
+import { formatPrice } from "formatting-service";
 
 const HomePage = () => {
+	const testNumber = 1.2323232323;
+	const formattedTestNumber = formatPrice({ num: testNumber });
 	return (
 		<div>
 			<Menu />
 			<p>Home</p>
+			<Tooltip
+				copy={testNumber.toString()}
+				content={testNumber}
+				placement="top"
+			>
+				<span className="flex gap-1 font-bold">
+					{formattedTestNumber}
+					<Icon type="COPY" className="w-4 stroke-black" />
+				</span>
+			</Tooltip>
 		</div>
 	);
 };
